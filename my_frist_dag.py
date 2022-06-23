b80bd31fe7e983db384e1fd46dc4a4bb7768362e
Vitor Albuquerque <94926140+vitorhugoma@users.noreply.github.com>
2022-06-23 08:47:22 -0300

    Create my_frist_dag.py

---
+++ my_frist_dag.py
@@ -0,0 +1,63 @@
+### Imports
+
+# import the libraries
+
+from datetime import timedelta
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+# Operators; we need this to write tasks!
+from airflow.operators.bash_operator import BashOperator
+# This makes scheduling easy
+from airflow.utils.dates import days_ago
+
+
+### DAG Arguments
+
+#defining DAG arguments
+
+# You can override them on a per-task basis during operator initialization
+
+default_args = {
+    'owner': 'Ramesh Sannareddy',
+    'start_date': days_ago(0),
+    'email': ['ramesh@somemail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+
+### DAG definition
+
+# define the DAG
+
+dag = DAG(
+    'my-first-dag',
+    default_args=default_args,
+    description='My first DAG',
+    schedule_interval=timedelta(days=1),
+)
+
+# define the tasks
+
+# define the first task named extract
+
+extract = BashOperator(
+    task_id='extract',
+    bash_command='cut -d":" -f1,3,6 /etc/passwd > extracted-data.txt',
+    dag=dag,
+)
+
+
+# define the second task named transform
+
+transform_and_load = BashOperator(
+    task_id='transform',
+    bash_command='tr ":" "," < extracted-data.txt > transformed-data.csv',
+    dag=dag,
+)
+
+
+# task pipeline
+extract >> transform_and_load
